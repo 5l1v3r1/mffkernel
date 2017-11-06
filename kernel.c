6c771c646eaaf191984efcec5c671207c3f6013e
Dario Clavijo <daedalus2027@gmail.com>
2017-11-06 19:41:22 -0300

    WIP: A20 Gate

--- kernel.c
+++ kernel.c
@@ -10,23 +10,28 @@ void cls(char *vidptr);
 void kprint(char *vidptr,char *str); 
 unsigned long strlen(const char* str);
 
-void kmain(void)
-{
+void kcheck_A20(){
 	const char *a20on = "A20 is enabled\n";
 	const char *a20off = "A20 is disabled\n";
-	const char *str = "My fucking first kernel\nDario Clavijo\n";
-
-	char *vidptr = (char*)0xb8000; 	//video mem begins here.
-
-	cls(vidptr);
-	kprint(vidptr,str);
 
 	if (check_A20() == 1) {
 		kprint(vidptr,a20on);
 	} else { 
 		kprint(vidptr,a20off); 
 	}
+}
 
+
+void kmain(void)
+{
+	const char *str = "My fucking first kernel\nDario Clavijo\n";
+	char *vidptr = (char*)0xb8000; 	//video mem begins here.
+
+	cls(vidptr);
+	kprint(vidptr,str);
+
+	kcheck_A20();
+	
 	//enable_A20();
 
 	return 0;
