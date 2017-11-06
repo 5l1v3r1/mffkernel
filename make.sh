d01de105f690d09b31878c7b40f5ae870cf1f2a9
Dario Clavijo <daedalus2027@gmail.com>
2017-11-06 18:38:51 -0300

    separate tty functions from kernel.c to tty.c

--- make.sh
+++ make.sh
@@ -2,4 +2,6 @@
 rm *.o
 nasm -f elf32 kernel.asm -o kasm.o
 gcc -m32 -c kernel.c -o kc.o
-ld -m elf_i386 -T kernel.ld -o kernel kasm.o kc.o
+gcc -m32 -c tty.c -o tty.o
+ld -m elf_i386 -T kernel.ld -o kernel kasm.o kc.o tty.o
+objdump -x kernel
