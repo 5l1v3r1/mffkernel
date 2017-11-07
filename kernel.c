9f35da6d922a68ae53d21b0730b7d3ad0c8ec366
Dario Clavijo <daedalus2027@gmail.com>
2017-11-07 16:30:36 -0300

    kernel.c rewrite

--- kernel.c
+++ kernel.c
@@ -2,6 +2,8 @@
 *  kernel.c
 */
 
+#include "io.h"
+
 void disable_ints();
 void enable_ints();
 int check_A20();
@@ -9,28 +11,37 @@ void enable_A20();
 void cls();
 void kprint(char *str); 
 unsigned long strlen(const char* str);
+char getScancode();
 
 void kcheck_A20()
 {
-	const char *a20on = "A20 is enabled\n";
-	const char *a20off = "A20 is disabled\n";
-
 	if (check_A20() == 1) {
-		kprint(a20on);
+		tty_print("A20 is enabled\n\r");
 	} else { 
-		kprint(a20off); 
+		tty_print("A20 is disabled\n\r"); 
 	}
 }
 
+void kspin(int spinCount){
+	for (int i = 0; i < spinCount; ++i)
+   		((void)0);  
+}
 
 void kmain(void)
 {
-	const char *str = "My fucking first kernel\nDario Clavijo\n";
 	cls();
-	kprint(str);
+	tty_print("My fucking first kernel\n\rAutor: Dario Clavijo\n\r");
 
 	kcheck_A20();
 	//enable_A20();
+	//gateA20();
+	//kcheck_A20();
 
+	tty_print("Spining...");
+	while (1){
+		kspin(1000);		
+		//tty_putchar(getScancode);
+	}
+	tty_print("End of the spinning..."); //it never should reach this point
 	return 0;
 }
