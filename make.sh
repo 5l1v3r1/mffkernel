c74fc158c66179e5446492c05e29641b72bbb475
Dario Clavijo <daedalus2027@gmail.com>
2017-11-07 12:45:35 -0300

    build dir

--- make.sh
+++ make.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 set -x
-rm *.o
-nasm -f elf32 a20.asm -o a20.o
-nasm -f elf32 kernel.asm -o kasm.o
-nasm -f elf32 ints.asm -o ints.o
-gcc -m32 -c kernel.c -o kc.o
-gcc -m32 -c tty.c -o tty.o
-ld -m elf_i386 -T kernel.ld -o kernel kasm.o ints.o a20.o kc.o tty.o
-objdump -x kernel
+
+BUILD=build
+mkdir $BUILD
+rm $BUILD/*.o
+
+nasm -f elf32 a20.asm -o $BUILD/a20.o
+nasm -f elf32 kernel.asm -o $BUILD/kasm.o
+nasm -f elf32 ints.asm -o $BUILD/ints.o
+gcc -m32 -c kernel.c -o $BUILD/kc.o
+gcc -m32 -c tty.c -o $BUILD/tty.o
+ld -m elf_i386 -T kernel.ld -o $BUILD/kernel $BUILD/kasm.o $BUILD/ints.o $BUILD/a20.o $BUILD/kc.o $BUILD/tty.o
+objdump -x $BUILD/kernel
